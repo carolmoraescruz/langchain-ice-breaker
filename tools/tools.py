4f6dbc1a971c6be4cfc59cb534072d6d5909077f
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2023-05-16 11:47:15 -0300

    creation of search tool

---
+++ tools/tools.py
@@ -0,0 +1,21 @@
+from langchain.serpapi import SerpAPIWrapper
+
+
+def get_profile_url(text: str) -> str:
+    """Searches for LinkedIn Profile Page.
+
+    Parameters
+    ----------
+    text : str
+        Name to be searched.
+
+    Returns
+    -------
+    str
+        LinkedIn Profile URL.
+    """
+
+    search = SerpAPIWrapper()
+    res = search.run(f"{text}")
+
+    return res
